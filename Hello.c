a7d708195bb56f5d3480bbef2873b5641cc32e1b
Kyle F <kylefitch@unomaha.edu>
2022-01-28 16:04:52 +0000

    added my major to the ouput

---
+++ Hello.c
@@ -0,0 +1,19 @@
+/**
+ * Name: Kyle Fitch
+ * Date: 2022/01/28
+ * Purpose: Program that prints name and major
+ * 
+ */
+ 
+ 
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(int argc, char **argv) {
+
+  printf("Kyle Fitch\n");
+  printf("Electrical Engineering");
+  
+
+  return 0;
+}
